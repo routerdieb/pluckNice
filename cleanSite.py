b2ef358296544f788589cf60ac5382987d2ca1bf
Michael <w.eso@live.de>
2022-05-11 10:55:52 +0200

    bugfix

--- cleanSite.py
+++ cleanSite.py
@@ -1,22 +1,30 @@
 import subprocess
 import sys
 
-
+#return true if something was returned
 def clean_site(name):
     result = subprocess.run(['pluck', 'export'], stdout=subprocess.PIPE)
     export = result.stdout.decode('utf-8')
+    is_removed = False
     for line in export.split("\r\n"):
         if (line.startswith('#') or line.startswith('flee') or line.startswith('block')):
             continue
         if(name in line):
             print(line)
             subprocess.run(['pluck', '-',line])
+            is_removed = True
+    return is_removed
 
 if __name__ == "__main__":
     result = subprocess.run(['pluck', 'version'], stdout=subprocess.PIPE)
     print("pluckeye version: " + result.stdout.decode('utf-8'))
 
+    has_removed_smth = False
     for i in range(1,len(sys.argv)):
-        clean_site(sys.argv[i])
+        if clean_site(sys.argv[i]):
+            has_removed_smth = True
 
-    print("the lines above have been removed")
+    if has_removed_smth:
+        print("the lines above have been removed")
+    else:
+        print("no entry in the configuration matched")
